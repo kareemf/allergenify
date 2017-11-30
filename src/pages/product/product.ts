70fc97a51f5f7f31549c6608f890ab63d16c84fd
Kareem Francis <francis.kareem@gmail.com>
2017-11-30 00:50:18 -0500

    Move photo taking logic from products to product

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavParams, ModalController } from 'ionic-angular';
+import { IonicPage, NavParams, ModalController, Platform } from 'ionic-angular';
 import { Product } from '../../models/product-model';
 import { Picture } from '../../models/picture-model';
 import { OcrProvider } from '../../providers/ocr/ocr';
@@ -7,6 +7,14 @@ import { AllergensProvider } from '../../providers/allergens/allergens';
 import { Allergen } from '../../models/allergen-model';
 import { GenericAlerter } from '../../providers/generic-alerter/generic-alerter';
 import { ProductProvider } from '../../providers/product/product';
+import { Camera } from '@ionic-native/camera';
+import { ImagePersistence } from '../../providers/image-persistence/image-persistence';
+
+enum CameraReadyStatus {
+  ViewDataNotLoaded = "Can't take photo - data not loaded",
+  NotOnDevice = "Can't take photo - not on device",
+  Ready = "Ready"
+}
 
 @IonicPage({
   defaultHistory: ['ProductsPage'],
@@ -17,12 +25,21 @@ import { ProductProvider } from '../../providers/product/product';
   templateUrl: 'product.html',
 })
 export class ProductPage {
-  private product: Product = Product.from({});
-  private onPicture: (product: Product) => void;
+  private static cameraOptions = {
+    quality: 100,
+    destinationType: 1, //return a path to the image on the device
+    sourceType: 1, //use the camera to grab the image
+    encodingType: 0, //return the image in jpeg format
+    cameraDirection: 1, //front facing camera
+    saveToPhotoAlbum: true //save a copy to the users photo album as well
+  };
 
-  constructor(private modalCtrl: ModalController, private navParams: NavParams, private ocrProvider: OcrProvider,
+  private isDataLoaded: boolean = false;
+  private product: Product = Product.from({});
+
+  constructor(private platform: Platform, private camera: Camera, private imagePersistence: ImagePersistence,
+              private modalCtrl: ModalController, private navParams: NavParams, private ocrProvider: OcrProvider,
               private productProvider: ProductProvider, private allergensProvider: AllergensProvider, private alerter: GenericAlerter,) {
-    this.onPicture = this.navParams.get('onPicture');
   }
 
   ionViewDidLoad() {
@@ -40,6 +57,7 @@ export class ProductPage {
   handleProductLoad(product: Product) {
     console.log(`loaded product ${product}`);
     this.product = product;
+    this.isDataLoaded = true;
   }
 
   private handleProductLoadError(error: any, id: string) {
@@ -51,21 +69,13 @@ export class ProductPage {
     console.error(logMessage, error);
 
     this.alerter.presentError(displasyMessasge);
+    this.isDataLoaded = true;
   }
 
   presentEdit(): void {
     this
       .alerter
       .presentRename(this.product, () => this.save());
-  }
-
-  addPicture(): void {
-    this.onPicture(this.product);
-  }
-
-  removePicture(picture: Picture): void {
-    this.product.removePicture(picture);
-    this.save();
   }
 
   save() {
@@ -77,6 +87,71 @@ export class ProductPage {
       .modalCtrl
       .create('PictureExtractedTextPage', { picture, product: this.product })
       .present();
+  }
+
+  removePicture(picture: Picture): void {
+    this.product.removePicture(picture);
+    this.save();
+  }
+
+  addPicture() {
+    if (this.cantTakePhoto()) {
+      this.alerter.presentError(this.cameraReadyStatus());
+      return false;
+    }
+
+    this
+      .camera
+      .getPicture(ProductPage.cameraOptions)
+      .then(imagePath => this.handleImageCapture(this.product, imagePath))
+      .catch(error => this.handleImageCaptureError(error))
+  }
+
+  private cantTakePhoto(): boolean {
+    return this.cameraReadyStatus() !== CameraReadyStatus.Ready;
+  }
+
+  private cameraReadyStatus(): CameraReadyStatus {
+    if(!this.isDataLoaded) {
+      return CameraReadyStatus.ViewDataNotLoaded;
+    }
+
+    if(!this.platform.is('cordova')){
+      return CameraReadyStatus.NotOnDevice;
+    }
+
+    return CameraReadyStatus.Ready;
+  }
+
+  private handleImageCapture(product: Product, imagePath: string): void {
+    console.log("captured image at path:", imagePath);
+
+    this
+      .imagePersistence
+      .persist(imagePath)
+      .then(persistentPath => this.handleImagePersistence(product, persistentPath))
+      .catch(error => this.handleImagePersistenceError(error));
+  }
+
+  private handleImagePersistence(product: Product, path: string): void {
+    console.log("image for", product, "persisted to path:", path);
+
+    const picture = new Picture(path);
+    product.addPicture(picture);
+
+    this.save();
+  }
+
+  private handleImagePersistenceError(error: any): void {
+    console.error("image persistence error:", error);
+
+    this.alerter.presentError('Failed to persist image on device');
+  }
+
+  private handleImageCaptureError(error: any): void {
+    console.error("error capturing image:", error);
+
+    this.alerter.presentError('Failed to capture image');
   }
 
   scanPicture(picture: Picture): void {
--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, Platform, NavController } from 'ionic-angular';
-import { Camera } from '@ionic-native/camera';
 import { ProductsProvider } from '../../providers/products/products';
 import { Product } from '../../models/product-model';
 import { ListPage } from '../list-page';
-import { ImagePersistence } from
 '../../providers/image-persistence/image-persistence';
-import { Picture } from '../../models/picture-model';
 import { GenericAlerter } from '../../providers/generic-alerter/generic-alerter';
-
-enum CameraReadyStatus {
-  ViewDataNotLoaded = "Can't take photo - data not loaded",
-  NotOnDevice = "Can't take photo - not on device",
-  Ready = "Ready"
-}
 
 @IonicPage()
 @Component({
@@ -21,18 +12,8 @@ enum CameraReadyStatus {
   templateUrl: 'products.html',
 })
 export class ProductsPage extends ListPage<Product> {
-  private static cameraOptions = {
-    quality: 100,
-    destinationType: 1, //return a path to the image on the device
-    sourceType: 1, //use the camera to grab the image
-    encodingType: 0, //return the image in jpeg format
-    cameraDirection: 1, //front facing camera
-    saveToPhotoAlbum: true //save a copy to the users photo album as well
-  };
-
   constructor(protected platform: Platform, productsProvider: ProductsProvider,
-              protected alerter: GenericAlerter,private navController: NavController,
-              private camera: Camera, private imagePersistence: ImagePersistence) {
+              protected alerter: GenericAlerter,private navController: NavController) {
     super('Product', platform, productsProvider, alerter);
   }
 
@@ -43,67 +24,6 @@ export class ProductsPage extends ListPage<Product> {
   view(product: Product): void {
     this.navController.push('ProductPage', {
       id: product.id,
-      onPicture: () => this.addPhotoTo(product)
     });
-  }
-
-  addPhotoTo(product: Product) {
-    if (this.cantTakePhoto()) {
-      this.alerter.presentError(this.cameraReadyStatus());
-      return false;
-    }
-
-    this
-      .camera
-      .getPicture(ProductsPage.cameraOptions)
-      .then(imagePath => this.handleImageCapture(product, imagePath))
-      .catch(error => this.handleImageCaptureError(error))
-  }
-
-  private cantTakePhoto(): boolean {
-    return this.cameraReadyStatus() !== CameraReadyStatus.Ready;
-  }
-
-  private cameraReadyStatus(): CameraReadyStatus {
-    if(!this.isListDataLoaded) {
-      return CameraReadyStatus.ViewDataNotLoaded;
-    }
-
-    if(!this.platform.is('cordova')){
-      return CameraReadyStatus.NotOnDevice;
-    }
-
-    return CameraReadyStatus.Ready;
-  }
-
-  private handleImageCapture(product: Product, imagePath: string): void {
-    console.log("captured image at path:", imagePath);
-
-    this
-      .imagePersistence
-      .persist(imagePath)
-      .then(persistentPath => this.handleImagePersistence(product, persistentPath))
-      .catch(error => this.handleImagePersistenceError(error));
-  }
-
-  private handleImagePersistence(product: Product, path: string): void {
-    console.log("image for", product, "persisted to path:", path);
-
-    const picture = new Picture(path);
-    product.addPicture(picture);
-
-    this.save();
-  }
-
-  private handleImagePersistenceError(error: any): void {
-    console.error("image persistence error:", error);
-
-    this.alerter.presentError('Failed to persist image on device');
-  }
-
-  private handleImageCaptureError(error: any): void {
-    console.error("error capturing image:", error);
-
-    this.alerter.presentError('Failed to capture image');
   }
 }
