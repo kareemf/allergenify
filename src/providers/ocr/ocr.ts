567c923368a28e72780af809beff7dbe003aea84
Kareem Francis <francis.kareem@gmail.com>
2017-11-30 00:21:58 -0500

    Minor cleanup

--- src/providers/ocr/ocr.ts
+++ src/providers/ocr/ocr.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Picture } from '../../models/picture-model';
 import { VISION_API_KEY  } from './access-config';
 
