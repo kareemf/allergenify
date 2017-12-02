2cd1cc78f91cbc3a9ca71674139d5c8b8e7597c1
Kareem Francis <francis.kareem@gmail.com>
2017-12-02 02:43:39 -0500

    Move interfaces below class

--- src/providers/ocr/ocr.ts
+++ src/providers/ocr/ocr.ts
@@ -3,6 +3,56 @@ import { HttpClient } from '@angular/common/http';
 import { Picture } from '../../models/picture-model';
 import { VISION_API_KEY  } from './access-config';
 
+@Injectable()
+export class OcrProvider {
+  private static BASE_VISION_API_URL= "https://vision.googleapis.com/v1/images:annotate?key=";
+
+  constructor(private httpClient: HttpClient) {
+    this.getDocumentTextRequestBody = this.getDocumentTextRequestBody.bind(this);
+    this.makeVisionRequest = this.makeVisionRequest.bind(this);
+    this.handleVisionResponse = this.handleVisionResponse.bind(this);
+  }
+
+  extractText(picture: Picture): Promise<string> {
+     const req = this.getDocumentTextRequestBody(picture.name);
+     return this.makeVisionRequest(req);
+  }
+
+  private getDocumentTextRequestBody(base64: string): VisionRequestBody {
+    return {
+      requests: [{
+        image: {
+          content: base64
+        },
+        features: [{
+          type: VisionFeatureType.DOCUMENT_TEXT_DETECTION,
+          maxResults: 1
+        }]
+      }]
+    }
+  }
+
+  private makeVisionRequest(requestBody): Promise<string> {
+    const url = this.visionApiUrl();
+
+    return this
+      .httpClient
+      .post(url, requestBody)
+      .toPromise()
+      .then(this.handleVisionResponse);
+  }
+
+  private visionApiUrl(): string {
+    return `${OcrProvider.BASE_VISION_API_URL}${VISION_API_KEY}`;
+  }
+
+  private handleVisionResponse(response: VisionResponse): string {
+    const { responses: [ firstResponse ] } = response;
+
+    return firstResponse.fullTextAnnotation.text;
+  }
+}
+
 // https://cloud.google.com/vision/docs/request
 interface VisionRequestBody {
   requests: AnnotateImageRequest[]
@@ -43,56 +93,3 @@ interface AnnotateImageResponse {
 interface TextAnnotation {
   text: string
 }
-
-@Injectable()
-export class OcrProvider {
-  private static BASE_VISION_API_URL= "https://vision.googleapis.com/v1/images:annotate?key=";
-
-  constructor(private httpClient: HttpClient) {
-    this.getDocumentTextRequestBody = this.getDocumentTextRequestBody.bind(this);
-    this.makeVisionRequest = this.makeVisionRequest.bind(this);
-    this.handleVisionResponse = this.handleVisionResponse.bind(this);
-  }
-
-  extractText(picture: Picture): Promise<string> {
-     const req = this.getDocumentTextRequestBody(picture.name);
-     return this.makeVisionRequest(req);
-
-  }
-
-  private getDocumentTextRequestBody(base64: string): VisionRequestBody {
-    return {
-      requests: [{
-        image: {
-          content: base64
-        },
-        features: [{
-          type: VisionFeatureType.DOCUMENT_TEXT_DETECTION,
-          maxResults: 1
-        }]
-      }]
-    }
-  }
-
-  private makeVisionRequest(requestBody): Promise<string> {
-    const url = this.visionApiUrl();
-
-    return this
-      .httpClient
-      .post(url, requestBody)
-      .toPromise()
-      .then(this.handleVisionResponse);
-  }
-
-  private visionApiUrl(): string {
-    return `${OcrProvider.BASE_VISION_API_URL}${VISION_API_KEY}`;
-  }
-
-  private handleVisionResponse(response: VisionResponse): string {
-    const { responses: [ firstResponse ] } = response;
-
-    return firstResponse.fullTextAnnotation.text;
-  }
-}
-
-
