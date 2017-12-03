53f085396f2efc0be5225380ee4eb8bc3a8fa087
Kareem Francis <francis.kareem@gmail.com>
2017-12-03 16:14:33 -0500

    First AllergensProvider test

--- src/providers/allergens/allergens-provider.spec.ts
+++ src/providers/allergens/allergens-provider.spec.ts
@@ -1,3 +1,26 @@
-describe('1st tests', () => {
-  it('true is true', () => expect(true).toBe(true));
+import { AllergensProvider } from './allergens-provider';
+import { Allergen } from '../../models/allergen-model';
+import { StorageMock } from '../../../test-config/stoage-mock';
+
+describe('AllergensProvider', () => {
+  let provider: AllergensProvider;
+  let allergen: Allergen;
+
+  beforeEach(() => {
+    allergen = Allergen.from({name: 'Foo'});
+    const storage = StorageMock.instance('allergens', [allergen]);
+
+    provider = new AllergensProvider(storage);
+  });
+
+  it('should flag word if there exists an allergen with that name', () => {
+    // given allergen "foo"
+    // when text is checked for allergens
+    provider
+    .checkForAllergens('Foo')
+    .then(results => {
+      // then text is matched
+      expect(results).toContain(allergen);
+    });
+  });
 });
