8c763732461c5a04374e028f0324a3b9c545fa89
Kareem Francis <francis.kareem@gmail.com>
2017-12-03 22:29:55 -0500

    Another test

--- src/models/product-model.spec.ts
+++ src/models/product-model.spec.ts
@@ -1,4 +1,5 @@
 import { Product, Status } from './product-model';
+import { Allergen } from './allergen-model';
 
 describe('Product', () => {
   it('should have a status of NotScanned if it has no scan date', () => {
@@ -10,7 +11,7 @@ describe('Product', () => {
     expect(status).toBe(Status.NotScanned);
   });
 
-  it('should have a status of NothingFound scanned w/ no allergens', () => {
+  it('should have a status of NothingFound if scanned w/ no allergens', () => {
     // given a scanned product w/ no allergens
     const product = Product.from({
       name: 'foo',
@@ -21,5 +22,18 @@ describe('Product', () => {
     const status = product.status;
     // it should refect that in status
     expect(status).toBe(Status.NothingFound);
+  });
+
+  it('should have a status of SomethingFound if scanned w/ allergens', () => {
+    // given a scanned product w/ no allergens
+    const product = Product.from({
+      name: 'foo',
+      dateScanned: new Date(),
+      allergens: [Allergen.from({ name: 'bar' })]
+    });
+    // when status is checked
+    const status = product.status;
+    // it should refect that in status
+    expect(status).toBe(Status.SomethingFound);
   });
 });
