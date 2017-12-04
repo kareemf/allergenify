dc91757695611b2d4abad3d57a3503f07a866ded
Kareem Francis <francis.kareem@gmail.com>
2017-12-03 22:28:08 -0500

    Another test

--- src/models/product-model.spec.ts
+++ src/models/product-model.spec.ts
@@ -9,4 +9,17 @@ describe('Product', () => {
     // it should refect that in status
     expect(status).toBe(Status.NotScanned);
   });
+
+  it('should have a status of NothingFound scanned w/ no allergens', () => {
+    // given a scanned product w/ no allergens
+    const product = Product.from({
+      name: 'foo',
+      dateScanned: new Date(),
+      allergens: []
+    });
+    // when status is checked
+    const status = product.status;
+    // it should refect that in status
+    expect(status).toBe(Status.NothingFound);
+  });
 });
