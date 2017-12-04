b6e8d971b05925a8f4e479258f9ac226ed7fc324
Kareem Francis <francis.kareem@gmail.com>
2017-12-03 22:40:13 -0500

    Dedupe tests

--- src/models/product-model.spec.ts
+++ src/models/product-model.spec.ts
@@ -9,39 +9,47 @@ describe('Product', () => {
     product = null;
   });
 
-  it('should have a status of NotScanned if it has no scan date', () => {
-    // given an unscanned product
-    product = Product.from({ name: 'foo' });
-    // when status is checked
-    status = product.status;
-    // it should refect that in status
-    expect(status).toBe(Status.NotScanned);
-  });
-
-  it('should have a status of NothingFound if scanned w/ no allergens', () => {
-    // given a scanned product w/ no allergens
-    product = Product.from({
-      name: 'foo',
-      dateScanned: new Date(),
-      allergens: []
+  describe('Status', () => {
+    beforeEach(() => {
+      status = null;
     });
-    // when status is checked
-    status = product.status;
-    // it should refect that in status
-    expect(status).toBe(Status.NothingFound);
-  });
 
-  it('should have a status of SomethingFound if scanned w/ allergens', () => {
-    // given a scanned product w/ no allergens
-    product = Product.from({
-      name: 'foo',
-      dateScanned: new Date(),
-      allergens: [Allergen.from({ name: 'bar' })]
+    it('should be NotScanned if it has no scan date', () => {
+      // given an unscanned product
+      product = Product.from({ name: 'foo' });
+      whenStatusIsChecked();
+      thenStatusShouldBe(Status.NotScanned);
     });
-    // when status is checked
-    status = product.status;
-    // it should refect that in status
-    expect(status).toBe(Status.SomethingFound);
+
+    it('should be NothingFound if scanned w/ no allergens', () => {
+      // given a scanned product w/ no allergens
+      product = Product.from({
+        name: 'foo',
+        dateScanned: new Date(),
+        allergens: []
+      });
+      whenStatusIsChecked();
+      thenStatusShouldBe(Status.NothingFound);
+    });
+
+    it('should be SomethingFound if scanned w/ allergens', () => {
+      // given a scanned product w/ no allergens
+      product = Product.from({
+        name: 'foo',
+        dateScanned: new Date(),
+        allergens: [Allergen.from({ name: 'bar' })]
+      });
+      whenStatusIsChecked();
+      thenStatusShouldBe(Status.SomethingFound);
+    });
+
+    function whenStatusIsChecked() {
+      status = product.status;
+    }
+
+    function thenStatusShouldBe(status: Status) {
+      expect(status).toBe(status);
+    }
   });
 
   it('should provide list of allergen names when asked', () => {
