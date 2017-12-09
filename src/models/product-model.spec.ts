4df60dea6690dfd2df6de0afc7bf5b3b126af4a7
Kareem Francis <francis.kareem@gmail.com>
2017-12-08 20:27:28 -0500

    Product status specs

--- src/models/product-model.spec.ts
+++ src/models/product-model.spec.ts
@@ -1,12 +1,87 @@
 import { Product } from './product-model';
 import { Allergen } from './allergen-model';
-import { Picture } from './picture-model';
+import { Picture, Status } from './picture-model';
+import { Stats } from 'fs';
 
 describe('Product', () => {
   let product: Product;
 
   beforeEach(() => {
     product = null;
+  });
+
+  describe('Status (Transient)', () => {
+    let status: Status;
+
+    beforeEach(() => {
+      status = null;
+    });
+
+    it('should be unscanned if no pictures', () => {
+      givenAProductWithoutPictures();
+      whenStatusIsChecked();
+      thenStatusShouldBe(Status.NotScanned);
+    });
+
+    it('should have the highest status of its pictures 1', () => {
+      givenAProductWithUnscannedPicture();
+      whenStatusIsChecked();
+      thenStatusShouldBe(Status.NotScanned);
+    });
+
+    it('should have the highest status of its pictures 2', () => {
+      givenAProductWithPicturesOfStatus(Status.NotScanned, Status.NothingFound);
+      whenStatusIsChecked();
+      thenStatusShouldBe(Status.NothingFound);
+    });
+
+    it('should have the highest status of its pictures 3', () => {
+      givenAProductWithPicturesOfStatus(Status.NotScanned, Status.NothingFound, Status.SomethingFound);
+      whenStatusIsChecked();
+      thenStatusShouldBe(Status.SomethingFound);
+    });
+
+    function givenAProductWithoutPictures() {
+      makeProduct([]);
+    }
+
+    function givenAProductWithUnscannedPicture() {
+      makeProduct([makePicture(Status.NotScanned)]);
+    }
+
+    function givenAProductWithPicturesOfStatus(...args) {
+      const pictures = args.map(stat => makePicture(stat));
+
+      makeProduct(pictures);
+    }
+
+    function makePicture(_status: Status) {
+      return Picture.from({
+        name: _status,
+        status: _status
+      });
+    }
+
+    function makeProduct(pictures) {
+      product = Product.from({
+        name: 'foo',
+        pictures
+      });
+    }
+
+    function whenStatusIsChecked() {
+      status = product.status;
+    }
+
+    function thenStatusShouldBe(status: Status) {
+      expect(status).toBe(status);
+    }
+  });
+
+  xdescribe('Date Scanned (Transient)', () => {
+    xit('should have the newest dateScaned of its pictures', () => {
+      // TODO
+    });
   });
 
   describe('Matching', () => {
@@ -35,12 +110,12 @@ describe('Product', () => {
 
   function givenScannedProductWithAllergens(): void {
     const picture  = Picture.from({
+      name: 'nikon',
       allergens: [Allergen.from({ name: 'bar' })]
     });
 
     product = Product.from({
       name: 'foo',
-      dateScanned: new Date(),
       pictures: [picture]
     });
   }
