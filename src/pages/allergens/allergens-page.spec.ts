c571ec287d8c75c352d19b2beef53b47dfd52793
Kareem Francis <francis.kareem@gmail.com>
2017-12-10 13:40:21 -0500

    Allergens page spec

---
+++ src/pages/allergens/allergens-page.spec.ts
@@ -0,0 +1,102 @@
+import { async, TestBed, ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
+
+import { IonicModule, Platform } from 'ionic-angular';
+
+import { Storage } from '@ionic/storage';
+import { StorageMock } from '../../../test-config/storage-mock';
+
+import { AllergensPage } from './allergens';
+import { PipesModule } from '../../pipes/pipes.module';
+import { AllergensProvider } from '../../providers/allergens/allergens-provider';
+import { Observable } from 'rxjs/Observable';
+import { Allergen } from '../../models/allergen-model';
+import { GenericAlerter } from '../../providers/generic-alerter/generic-alerter';
+import { PlatformMock } from '../../../test-config/mocks-ionic';
+
+import { GenericAlerterMock } from '../../../test-config/generic-alerter-mock';
+import { AllergensProviderMock } from '../../../test-config/allergens-provider-mock';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+describe('AllergensPage', () => {
+  let fixture: ComponentFixture<AllergensPage>;
+  let component: AllergensPage;
+  let debugElement: DebugElement;
+  let nativeElement: HTMLElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AllergensPage],
+      imports: [
+        IonicModule.forRoot(AllergensPage),
+        PipesModule,
+      ],
+      providers: [
+        { provide: Storage, useClass: StorageMock },
+        { provide: Platform, useClass: PlatformMock },
+        { provide: AllergensProvider, useClass: AllergensProviderMock },
+        { provide: GenericAlerter, useClass: GenericAlerterMock },
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AllergensPage);
+    component = fixture.componentInstance;
+  });
+
+  it('should be created', () => {
+    expect(component instanceof AllergensPage).toBe(true);
+  });
+
+  it('should load items when platform is ready', fakeAsync(() => {
+    givenStoredAllergensNumbering(3);
+    andALoadedView();
+    whenListElementIsGrabbed();
+    thenCountIs(3);
+  }));
+
+  function givenStoredAllergensNumbering(count: number) {
+    const allergens = makeNumberOfAllergens(count);
+    const allergensProvider = getAllergensProviderInstance();
+
+    // @ts-ignore: Property 'items' is missing in type 'AllergensProvider'.
+    const allergensProviderMock = allergensProvider as AllergensProviderMock;
+
+    allergensProviderMock.setItems(allergens);
+  }
+
+  function makeNumberOfAllergens(count: number) {
+    const allergens = [];
+
+    for (let i = 0; i < count; i++) {
+      allergens.push(Allergen.from({ name: i }));
+    }
+
+    return allergens;
+  }
+
+  function getAllergensProviderInstance() {
+    return fixture.debugElement.injector.get(AllergensProvider);
+  }
+
+  function andALoadedView() {
+    component.ionViewDidLoad();
+    updateState();
+  }
+
+  function updateState() {
+    fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
+  }
+
+  function whenListElementIsGrabbed() {
+    debugElement = fixture.debugElement.query(By.css('.list'));
+    nativeElement = debugElement.nativeElement;
+  }
+
+  function thenCountIs(expectedCount: number) {
+    expect(nativeElement.children.length).toBe(expectedCount);
+  }
+});
